801f4683854f916d8861104cfc74be28f8830e32
Marwan09 <75195799+Marwan09@users.noreply.github.com>
2023-12-16 13:07:16 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const div_pas=document.getElementById('div_pas')
+// div_pas.style.backgroundImage="linear-gradient(#FFC7C7,#ffff)";
+const input_id = document.getElementById('input_id')
+// input_id.style.backgroundColor="red";
+const hide_show=document.getElementById('hide_show');
+// hide_show.style.backgroundColor="red"
+// input_class.style.backgroundImage="linear-gradient(#FFC7C7,#fjff)";
+hide_show.onclick=function(){
+    if(input_id.type==="password"){
+        input_id.type="text"
+    }else input_id.type="password"
+    
+}
+
+  
+
+
+// input_pass.type = "password"
+// js for hide-show password
+
+// var input_pass = document.getElementById("input-pass");
+// var hide_show = document.getElementById("hide-show");
+
+// hide_show.onclick = function(){
+//     if(input_pass.type == "password"){
+//         input_pass.type = "text";
+//     }
+//     else{
+// //         input_pass.type = "password"
+//     }
+// }
+
